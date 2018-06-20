40223c593f937bb78120fa7079f5d86b501a8e4d
Chris Crawford <chris.crawford@robotsandpencils.com>
2018-06-20 14:02:31 -0400

    OOTB .netcore mvc project template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnet_vue_poc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
